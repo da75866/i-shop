9bd174381ff9616614ef14d47029ac66fefe2337
Double Agent <da75866@gmail.com>
2021-05-02 00:58:09 +0300

    database created and connected

--- Database/Product.php
+++ Database/Product.php
@@ -1,6 +1,13 @@
 <?php
 
+// use to fetch product data
 class Product
 {
-    
+    public $db = null;
+
+    public function __construct(DBController $db)
+    {
+        if(!isset($db->con)) return null;
+        $this->$db = $db;
+    }
 }
---
+++ functions.php
@@ -0,0 +1,6 @@
+<?php
+    require_once('./Database/DBController.php');
+
+//DBController object
+$db = new DBController();
+
